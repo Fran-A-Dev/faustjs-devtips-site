40fb293c24575990f9a9e2fbd57acadfa26903b5
Francis Agulto <francis.agulto@wpengine.com>
2021-09-02 09:29:16 -0500

    added submit mutation

--- pages/termSubmit.tsx
+++ pages/termSubmit.tsx
@@ -1,24 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/lib/Layout";
 import styles from "../styles/pages/Termsubmit.module.scss";
 import { client } from "../client";
 
+export interface FormData {
+  name: string;
+  email: string;
+  topic: string;
+}
+
 export default function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [topic, setTopic] = useState("");
+  const [message, setMessage] = useState<string | undefined>();
+  const [submitContactFormCF, { data, error, isLoading }] = client.useMutation(
+    (mutation, args: FormData) => {
+      console.log("mutation event");
+      console.log("args", args);
+      const { successMessage, name, errors } = mutation.submitContactFormCF({
+        input: args,
+      });
+
+      console.log(name);
+      console.log(successMessage);
+
+      return successMessage;
+    }
+  );
+
+  const errorMessage = error?.message;
+
+  const SubmitForm = async (event) => {
+    event.preventDefault();
+
+    console.log("submit form event");
+
+    submitContactFormCF({
+      args: { name, email, topic },
+    }).catch(console.error);
+  };
+
   return (
     <>
       <Layout>
-        <main className={`content content-index ${styles.contact} `}>
-          <div>
-            <h1>Submit a Subject or Topic to Learn!</h1>
-            <form action="">
+        <main className={`content content-index ${styles.contact}`}>
+          <div className="wrap">
+            <h1>Submit a Term</h1>
+            <div className="message">{message}</div>
+            <form style={{ width: "300px" }}>
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" />
+              <input
+                value={name}
+                type="text"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+              />
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" />
+              <input
+                value={email}
+                type="text"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <label htmlFor="topic">Topic</label>
-              <input type="text" name="Topic" />
+              <input
+                value={topic}
+                type="text"
+                name="topic"
+                onChange={(e) => setTopic(e.target.value)}
+              />
               <br></br>
-              <button className={styles.button}>Submit Here</button>
+              <button
+                className="button"
+                disabled={isLoading}
+                onClick={SubmitForm}
+              >
+                Submit
+              </button>
             </form>
           </div>
         </main>
